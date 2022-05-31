6de470b9b07b2617052caa550d853ff81ebbae6d
jjoejacob <63254154+jjoejacob@users.noreply.github.com>
2022-05-31 07:41:57 -0500

    Create Search.cs

---
+++ Search.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Linq;
+
+namespace InterviewTest
+{
+    public class Program
+    {
+        
+        public static void Main()
+        {
+            SearchList.Search();
+        } 
+
+             
+    }
+
+    public class SearchList
+    {
+        void Search(int[] lst,int target)
+        {
+           //Print 2 numbers from lst whoose sum equals to target
+        }
+    }
